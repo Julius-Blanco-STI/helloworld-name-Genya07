7dffa82637a23fe15686c65d34cfb662474aaf86
Kurt_Galang <58575739+Genya07@users.noreply.github.com>
2019-12-06 12:11:52 +0800

    Update Main.java
    @juliusb0205 
    Final

--- Main.java
+++ Main.java
@@ -5,15 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner Obj = new Scanner(System.in);
         System.out.println("Hello World");
-    
         System.out.println("What is your name?");
         String VarName = Obj.nextLine();
         System.out.println("What is your address?");
         String VarAdd = Obj.nextLine();
         System.out.println("What is your age?");
         int VarAge = Obj.nextInt();
-    
-        
         System.out.println("Hi " + VarName + ", Your age is " + VarAge + " and you live at " + VarAdd);
     
     }
