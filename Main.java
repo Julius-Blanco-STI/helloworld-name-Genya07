fc69cef77fb0abffceef224bd6892a7367800bb6
Kurt_Galang <58575739+Genya07@users.noreply.github.com>
2019-12-06 08:36:37 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+package main;
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public static void main(String[] args) {
+        
+        String VarName = "Kurt";
+        
+            System.out.println("Hello World " + VarName);
+    }
+    
 }
