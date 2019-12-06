a83ea30aec21b54c8263b8af1ffbbbf0e7fe124a
Kurt_Galang <58575739+Genya07@users.noreply.github.com>
2019-12-06 09:01:32 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
+package main;
+import java.util.*;
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public static void main(String[] args) {
+        Scanner Obj = new Scanner(System.in);
+        System.out.println("Hello World");
+    
+    System.out.println("What is your name?");
+        String VarName = Obj.nextLine();
+    System.out.println("What is your address?");
+        String VarAdd = Obj.nextLine();
+    System.out.println("What is your age?");
+        int VarAge = Obj.nextInt();
+    
+        
+    System.out.println("Hi " + VarName +"!");
+    System.out.println("Your age is " + VarAge);
+    System.out.println("You live at" + VarAdd);
+    
+    }
+    
 }
